d5424a2ab50f0725ba5eea66f2a3be23c528572b
Funky Brows <gravity_black@mailbox.org>
2022-11-18 13:12:20 -0500

    test: add_torrent

---
+++ src/tests/test_core.py
@@ -0,0 +1,20 @@
+import base64
+import pytest
+
+import os
+import os.path
+
+from deluge_control.client import DelugeClient
+
+@pytest.fixture(scope="session")
+def deluge_client():
+    return DelugeClient()
+
+def test_add_torrent(deluge_client):
+    torrent_id = deluge_client.add_torrent_file_async('alice', 'tests/data/alice.torrent', download_location="/home/cappytwoknees/Remote", move_completed=False)
+    try:
+        assert torrent_id
+    finally:
+        deluge_client.remove_torrent(torrent_id, remove_data=False)
+
+
