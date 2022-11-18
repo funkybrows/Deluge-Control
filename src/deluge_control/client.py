fb5cf4d848c8d52c514d2f92c6140122adbf5c8c
Funky Brows <gravity_black@mailbox.org>
2022-11-18 13:13:28 -0500

    feat: add torrent file asynchronously

---
+++ src/deluge_control/client.py
@@ -0,0 +1,28 @@
+import base64
+from sonarr_announced import deluge
+
+import os
+import os.path
+
+class DelugeClient():
+    POSSIBLE_ADD_OPTIONS = {
+        "download_location",
+        "move_completed"
+    }
+    def __init__(self):
+        self.client = deluge.get_deluge_client()
+    
+    def add_torrent_file_async(self, name, file_path, **options):
+        with open(file_path, 'rb') as f:
+            content = f.read()
+        encoded_content = base64.b64encode(content)
+
+        add_options = {}
+        if options:
+            for possible_add_option in self.POSSIBLE_ADD_OPTIONS:
+                if option_value:= options.get(possible_add_option):
+                    add_options[possible_add_option] = option_value
+
+        result = self.client.call('core.add_torrent_file_async', name, encoded_content, add_options)
+        return result
+
