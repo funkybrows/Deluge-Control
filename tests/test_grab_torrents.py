22e01104d2614c427769327a83c8a097af3344bc
Funky Brows <gravity_black@mailbox.org>
2022-11-20 22:33:05 -0500

    chore: add time_added to get_torrents_with

--- tests/test_grab_torrents.py
+++ tests/test_grab_torrents.py
@@ -18,8 +18,26 @@ def get_torrents_by_id(n_torrents):
     }
 
 
-def get_torrents_with(n_torrents, name_options=None, state_options=None):
+def get_torrents_with(
+    n_torrents, name_options=None, state_options=None, torrent_dt=None
+):
     ret = {}
+    if not torrent_dt or isinstance(torrent_dt, dt.datetime):
+        use_dt = torrent_dt if torrent_dt else dt.datetime.utcnow()
+        epoch = str(use_dt.timestamp()).encode("utf-8")
+        torrent_dts = [epoch] * n_torrents
+    elif isinstance(torrent_dt, (set, list, tuple)) and isinstance(
+        torrent_dt[0], dt.datetime
+    ):
+        torrent_dts = torrent_dt
+        torrent_dts = [
+            str(torrent_dt.timestamp()).encode("utf-8") for torrent_dt in torrent_dts
+        ]
+    else:
+        raise ValueError(
+            f"torrent_dt ({torrent_dt}) must be None, dt, or iterable of dts"
+        )
+
     if name_options:
         name_options = random.sample(name_options, n_torrents)
     for i, torrent_id in enumerate(get_torrents_by_id(n_torrents)):
@@ -30,6 +48,7 @@ def get_torrents_with(n_torrents, name_options=None, state_options=None):
             )
         if name_options:
             torrent_info["name".encode("utf-8")] = name_options[i].encode("utf-8")
+        torrent_info["time_added".encode("utf-8")] = torrent_dts[i]
     return ret
 
 
