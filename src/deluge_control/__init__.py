fcf0cb6e73a4dd11998e2adf3a9280707b106e53
Funky Brows <gravity_black@mailbox.org>
2022-11-29 15:19:49 -0500

    fix: path to logging file should be relative in order to run in pytest and main

--- src/deluge_control/__init__.py
+++ src/deluge_control/__init__.py
@@ -1,3 +1,8 @@
+from pathlib import Path
+from os import path
 import logging.config
 
-logging.config.fileConfig("config/logging.conf")
+log_file_path = path.join(
+    Path(__file__).parent.parent.absolute(), "config", "logging.conf"
+)
+logging.config.fileConfig(log_file_path)
