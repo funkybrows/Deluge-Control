c6281c75cdb8a9993a5c827c60a162a23ff83b38
Funky Brows <gravity_black@mailbox.org>
2022-12-17 16:12:00 -0500

    feat: add AioPika client for downloading torrents

---
+++ src/deluge_control/rabbit.py
@@ -0,0 +1,34 @@
+import json
+import logging
+import os
+
+from aio_pika_wrapper.client import AioClient, AsyncState
+
+from deluge_control.client import get_deluge_client
+
+logger = logging.getLogger("deluge.rpc")
+
+
+class AioDownloader(AioClient):
+    TIMEOUT = 5
+
+    def __init__(
+        self,
+        exchange_name=f"{os.environ.get('PROJECT_NAME')}-{os.environ.get('NAMESPACE')}",
+        client_name=None,
+        pool=None,
+    ):
+        super().__init__(None, client_name, pool=pool)
+        self._exchange_name = exchange_name
+
+    async def connect(self):
+        await super().connect()
+        await self.wait_until_connected(timeout=self.TIMEOUT)
+        exchange = await self.declare_exchange(
+            self._exchange_name,
+            exchange_type="topic",
+            durable=True,
+            timeout=self.TIMEOUT,
+        )
+        if exchange:
+            await self.set_exchange(self._exchange_name)
