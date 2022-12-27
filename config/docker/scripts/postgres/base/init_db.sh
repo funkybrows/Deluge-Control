713bbcc985175d0a49ec7d402a0994a81334fb61
Funky Brows <gravity_black@mailbox.org>
2022-12-27 18:56:38 -0500

    fix: don't force usage of init_db.sh by setting it by default in local

--- config/docker/scripts/postgres/base/init_db.sh
+++ config/docker/scripts/postgres/base/init_db.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	CREATE DATABASE $PG_NAME \
-        with OWNER=$POSTGRES_USER;
+psql -v ON_ERROR_STOP=1 --username "$PG_USER" <<-EOSQL
+	CREATE DATABASE deluge_control \
+        with OWNER=$PG_USER;
 EOSQL
--- config/docker/scripts/postgres/init_db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$PG_USER" <<-EOSQL
-	CREATE DATABASE deluge_control \
-        with OWNER=$PG_USER;
-EOSQL
