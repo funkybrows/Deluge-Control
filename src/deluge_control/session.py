14b532cb52324d227378982698e03fe1f79c39aa
Funky Brows <gravity_black@mailbox.org>
2022-12-28 12:25:00 -0500

    refactor: don't have two (get_engine)s

--- src/deluge_control/session.py
+++ src/deluge_control/session.py
@@ -3,10 +3,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-def get_engine(echo=False):
+def get_engine(echo=False, pg_name=os.environ.get("PG_NAME")):
     return create_engine(
         f"postgresql+{os.environ.get('PG_DRIVER')}://{os.environ.get('PG_USER')}"
-        f":{os.environ.get('PG_PASSWORD')}@{os.environ.get('PG_HOST')}:{os.environ.get('PG_PORT', 5432)}/{os.environ.get('PG_NAME')}",
+        f":{os.environ.get('PG_PASSWORD')}@{os.environ.get('PG_HOST')}:{os.environ.get('PG_PORT', 5432)}/{pg_name}",
         echo=echo,
         future=True,
     )
--- tests/conftest.py
+++ tests/conftest.py
@@ -6,30 +6,25 @@ import pytest
 from sqlalchemy import create_engine
 from deluge_control.client import DelugeClient
 from deluge_control.models import Base
-from deluge_control.session import get_session
+from deluge_control.session import get_session, get_engine
 
 fake = faker.Faker()
 
 video_qualities = ["480p", "720p", "1080p", "2160p"]
 
 
-def get_engine(echo=True):
-    return create_engine(
-        f"postgresql+{os.environ.get('PG_DRIVER')}://{os.environ.get('PG_USER')}"
-        f":{os.environ.get('PG_PASSWORD')}@{os.environ.get('PG_HOST')}:{os.environ.get('PG_PORT', 5432)}/test-{os.environ.get('PG_NAME')}",
-        echo=echo,
-        future=True,
-    )
+def get_test_engine(echo=True):
+    return get_engine(echo=echo, pg_name=f"test-{os.environ.get('PG_NAME')}")
 
 
 @pytest.fixture(scope="module")
 def session():
-    return get_session(get_engine())
+    return get_session(get_test_engine())
 
 
 @pytest.fixture
 def setup_database():
-    Base.metadata.bind = get_engine()
+    Base.metadata.bind = get_test_engine()
     Base.metadata.create_all()
     yield
 
