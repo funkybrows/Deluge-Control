8446eaae05dbbb35ac1181086fdddd111a96df0e
Funky Brows <gravity_black@mailbox.org>
2022-12-28 12:04:37 -0500

    fix: don't use expired sessions during tests

--- tests/test_check_torrents.py
+++ tests/test_check_torrents.py
@@ -25,14 +25,25 @@ def test_check_torrent(deluge_client, movie_names, db_5_sessions):
     with patch_torrents_status() as mock_torrents:
         mock_torrents.return_value = get_torrents_with(1, movie_names[1], ("Seeding",))
         register_new_torrents(deluge_client, db_5_sessions[0])
-        torrent = db_5_sessions[0].execute(select(Torrent)).scalars().all()[0]
-        assert not db_5_sessions[0].execute(select(TorrentSnapshot)).scalars().all()
-        mock_torrents.return_value = get_snapshot_info((torrent.torrent_id,))
-        checked_torrents = check_seeding_torrents(deluge_client, db_5_sessions[1])
-        assert checked_torrents[0] in (
-            db_5_sessions[1]
-            .execute(select(TorrentSnapshot).where(TorrentSnapshot.torrent == torrent))
-            .scalars()
-            .all()
+        torrent = db_5_sessions[1].execute(select(Torrent)).scalars().all()[0]
+        assert not db_5_sessions[1].execute(select(TorrentSnapshot)).scalars().all()
+        mock_torrents.return_value = get_check_torrents_info(
+            (torrent.torrent_id,), ("Seeding",)
+        )
+        checked_torrents = check_seeding_torrents(deluge_client, db_5_sessions[2])
+        assert (expected_id := checked_torrents[0].torrent.torrent_id) in (
+            db_returned_ids := [
+                row.torrent_id
+                for row in db_5_sessions[3]
+                .execute(
+                    select(Torrent.torrent_id)
+                    .select_from(TorrentSnapshot)
+                    .join(TorrentSnapshot.torrent)
+                    .where(TorrentSnapshot.torrent == torrent)
+                )
+                .all()
+            ]
+        ), f"expected_id: {expected_id}, not in {db_returned_ids}"
+        assert checked_torrents[0].torrent.next_check_time > dt.datetime.utcnow()
         )
         assert checked_torrents[0].torrent.next_check_time > dt.datetime.utcnow()
