cea8b3f216638801a7d133872b6e156ccc1f95d2
Funky Brows <gravity_black@mailbox.org>
2022-12-28 17:47:25 -0500

    fix: add delay before downloading to avoid unregistered torrents

--- src/deluge_control/rabbit.py
+++ src/deluge_control/rabbit.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 import logging
 import os
@@ -53,6 +54,7 @@ class AioDownloader(AioClient):
                     )
                     try:
                         deluge_client = get_deluge_client()
+                        await asyncio.sleep(10)
                         deluge_client.add_torrent_url(
                             message_info["torrent_url"],
                             **message_info.get("download_options", {}),
