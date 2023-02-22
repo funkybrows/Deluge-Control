e6688b036e6b6cf7a5b39b517c1d46a178db7f1d
Funky Brows <gravity_black@mailbox.org>
2023-02-22 18:54:50 -0500

    test: check all torrents

--- tests/test_check_torrents.py
+++ tests/test_check_torrents.py
@@ -429,15 +429,23 @@ def test_change_seeding_to_paused(deluge_client, movie_names, db_5_sessions):
             assert torrent.state == StateChoices.PAUSE and torrent.name == torrent_name
 
 
-def test_change_seeding_to_deleted(deluge_client, movie_names, db_5_sessions):
-    with patch_torrents_status() as mock_torrents:
-        mock_torrents.return_value = get_torrents_with(1, movie_names[:1], ("Seeding",))
-        register_new_torrents(deluge_client, db_5_sessions[0])
-        torrent_name, torrent_state = (
-            db_5_sessions[1].execute(select(Torrent.name, Torrent.state)).all()[0]
-        )
-        assert torrent_name and torrent_state == StateChoices.SEED
-        mock_torrents.return_value = {}
-        check_seeding_torrents(deluge_client, db_5_sessions[2])
-        torrent = db_5_sessions[3].execute(select(Torrent)).scalars().first()
-        assert torrent.state == StateChoices.DEL and torrent.name == torrent_name
+@mock.patch("deluge_control.check_torrents.check_seeding_torrents")
+@mock.patch("deluge_control.check_torrents.check_downloading_torrents")
+@mock.patch("deluge_control.check_torrents.update_torrent_states")
+@mock.patch("deluge_control.client.DelugeClient.get_torrents_status")
+def test_check_torrents(
+    mock_get_status,
+    mock_update_state,
+    mock_check_downloading_torrents,
+    mock_check_seeding_torrents,
+    deluge_client,
+    db_session,
+):
+    """
+    Assert expected calls are made in this conglomerate function
+    """
+    check_torrents(deluge_client, db_session)
+    mock_get_status.assert_called_once()
+    mock_update_state.assert_called_once()
+    mock_check_downloading_torrents.assert_called_once()
+    mock_check_seeding_torrents.assert_called_once()
