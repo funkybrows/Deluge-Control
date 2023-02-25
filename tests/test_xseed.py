2c06991accf6cd3a4eb429a5783199fa96b12070
Funky Brows <gravity_black@mailbox.org>
2023-02-25 17:06:45 -0500

    test: generate XSeed as necessary

--- tests/test_xseed.py
+++ tests/test_xseed.py
@@ -1,3 +1,5 @@
+import random
+import pytest
 from sqlalchemy.sql.expression import select
 from deluge_control.check_torrents import (
     check_seeding_torrents,
@@ -11,18 +13,57 @@ from utils import (
 )
 
 
-def test_generate_xseed(deluge_client, movie_names, db_5_sessions):
+def get_x_seed_query(torrent_id):
+    return (
+        select(TorrentXSeed)
+        .join(Torrent.xseeds)
+        .where(Torrent.torrent_id == torrent_id)
+    )
+
+
+@pytest.fixture
+def seeding_torrent(movie_names):
+    torrent_id = "abcd1234"
+    return Torrent(
+        torrent_id=torrent_id, name=random.choice(movie_names), state=StateChoices.SEED
+    )
+
+
+def test_generate_xseed(deluge_client, db_5_sessions, seeding_torrent):
     """
     If one doesn't exist, we should generate an xseed obj for a seeding
     torrent.
     """
     iter_sessions = iter(db_5_sessions)
-    torrent_id = "abc123"
     with next(iter_sessions) as db_session:
-        torrent = Torrent(
-            torrent_id=torrent_id, name=movie_names[0], state=StateChoices.SEED
-        )
-        db_session.add(torrent)
+        db_session.add(seeding_torrent)
+        db_session.commit()
+
+    with patch_torrents_status() as mock_status:
+        with next(iter_sessions) as db_session:
+            db_session.add(seeding_torrent)
+            mock_status.return_value = get_seeding_torrents_info(
+                (seeding_torrent.torrent_id,)
+            )
+            assert not (
+                db_session.execute(get_x_seed_query(seeding_torrent.torrent_id))
+                .scalars()
+                .first()
+            )
+            db_seeding_torrents = split_ready_db_torrents_by_state(db_session)[
+                StateChoices.SEED
+            ]
+            client_torrents = deluge_client.decode_torrent_data(
+                deluge_client.get_torrents_status(*TORRENTS_STATUS_DEFAULT_ARGS)
+            )
+            check_seeding_torrents(db_session, db_seeding_torrents, client_torrents)
+            assert (
+                db_session.execute(get_x_seed_query(seeding_torrent.torrent_id))
+                .scalars()
+                .first()
+            )
+
+
         db_session.commit()
 
     with patch_torrents_status() as mock_status:
